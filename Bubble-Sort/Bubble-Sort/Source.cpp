0e741c499b763e3785c4306d3f2047796ef0cefd
Thomas Arsanious <tomyarsanious@gmail.com>
2026-05-11 14:29:02 +0300

    implemented as a Function

--- Bubble-Sort/Bubble-Sort/Source.cpp
+++ Bubble-Sort/Bubble-Sort/Source.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 using namespace std;
 
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
 int main() {
     int n;
 
@@ -14,15 +26,7 @@ int main() {
         cin >> arr[i];
     }
 
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = 0; j < n - i - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
-                int temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-            }
-        }
-    }
+    bubbleSort(arr, n);
 
     cout << "Sorted array:\n";
     for (int i = 0; i < n; i++) {
